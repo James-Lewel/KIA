5f803a3edba829285e2d3eedda4971482eba92c9
James-Lewel <jameslewel.padecio@cit.edu>
2024-05-31 16:01:35 +0800

    Update depth estimation

--- kia_ai.py
+++ kia_ai.py
@@ -16,12 +16,13 @@ class ObjectDetection:
         self.rf = Roboflow(api_key="7gqyul75jvef76MNUFCI")
         #self.project = self.rf.workspace("stust-k-lab").project("kitchen-intelligent-assistant")
         #self.model = self.project.version(14).model
+        #self.depth_model = Midas_Depth()
+        #self.stereo_camera = StereoCamera()
+        
         self.main_dish_count = 0
         self.side_dish_count = 0
         self.width = 960
         self.height = 540
-        self.depth_model = Midas_Depth()
-        self.stereo_camera = StereoCamera()
 
     def get_class_color(self, class_name):
         if "-m-" in class_name:
@@ -37,34 +38,71 @@ class ObjectDetection:
             elif self.side_dish_count == 3:
                 return (0, 255, 255)  # Yellow
         return (255, 255, 255)  # Default to white for unknown classes
-
-    def estimate_proportions(self, frame):
-        # Perform edge detection
-        gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        edges = cv2.Canny(gray_frame, 100, 200)
-
-        # Obtain depth map
-        #depth_map = self.stereoCamera.getDepth(frame)
+    
+    def estimate_depth(self, frame):
+        depth_map = self.stereoCamera.getDepth(frame)
         depth_map = self.depth_model.get_depth_image(frame)
-        #threshold = np.percentile(depth_map, 50) 
-
-        # Apply a mask using the depth map to focus only on relevant areas
-        #masked_depth_map = np.where(depth_map < threshold, 1, 0)
-        #masked_edges = cv2.bitwise_and(edges, edges, mask=(masked_depth_map * 255).astype(np.uint8))
-
-        #kernel = np.ones((5, 5), np.uint8)
-        #dilated_edges = cv2.dilate(masked_edges, kernel, iterations=1)
-
-        #filled_areas = dilated_edges
-        #filled_pixels = cv2.countNonZero(filled_areas)
-        #total_pixels = frame.shape[0] * frame.shape[1]
-        #proportion = 100 * (filled_pixels / total_pixels)
-
-        #cv2.imshow('filled', filled_areas)
+        cv2.imshow('filled', filled_areas)
         cv2.imshow('depth map', (depth_map * 255).astype(np.uint8))
         cv2.waitKey()
 
-        #return proportion
+    def estimate_proportions(self, frame):
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+        edges = cv2.Canny(blurred, 50, 150)
+        
+        contours, _ = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        mask = np.zeros_like(frame)
+        cv2.drawContours(mask, contours, -1, (255, 255, 255), thickness=cv2.FILLED)
+        
+        food_pixels = np.count_nonzero(mask)
+        total_pixels = frame.size // 3 
+        
+        food_proportion = food_pixels / total_pixels
+        print(f'Proportion of food left: {food_proportion:.2f}')
+        
+        # Display the mask for visualization
+        cv2.imshow('Mask', mask)
+        cv2.waitKey(0)
+        cv2.destroyWindow('Mask')
+        
+        return food_proportion
+    
+    #def estimate_proportions(self, frame):
+    #    mask_frame = frame.copy()
+    #    
+    #    lower_white = np.array([128, 128, 128], dtype=np.uint8)  # #c1baac
+    #    upper_white = np.array([255, 255, 255], dtype=np.uint8)  # #ffffff
+    #    
+    #    mask = cv2.inRange(mask_frame, lower_white, upper_white)
+    #    mask_frame[mask == 255] = [0, 0, 0]
+    #    
+    #    total_pixels = mask_frame.size // 3
+    #    empty_pixels = np.sum(mask == 255)
+    #    food_proportion = (total_pixels - empty_pixels) / total_pixels
+    #    
+    #    print(f'Proportion of food left: {food_proportion:.2f}')
+    #    
+    #    cv2.imshow('Shaded Frame', mask_frame)
+    #    cv2.waitKey(0)
+    #    cv2.destroyWindow('Shaded Frame')
+    #    
+    #    return food_proportion
+
+    #def estimate_proportions(self, frame):
+    #    # Obtain depth map
+    #    #depth_map = self.stereoCamera.getDepth(frame)
+    #    #depth_map = self.depth_model.get_depth_image(frame)
+    #    depth_map = (frame * 255. / 2.6842105263157894).astype(np.uint8)
+    #    depth_map = cv2.applyColorMap(depth_map, cv2.COLORMAP_JET)
+    #    np.ascontiguousarray(depth_map)
+    #    
+    #    #cv2.imshow('filled', filled_areas)
+    #    #cv2.imshow('depth map', (depth_map * 255).astype(np.uint8))
+    #    cv2.imshow('depth map', depth_map)
+    #    cv2.waitKey()
+    #
+    #    #return proportion
 
     
     def process_yolov8(self, frame):
@@ -103,13 +141,12 @@ class ObjectDetection:
             cv2.rectangle(frame, (x1, y1 - text_size[1]), (x1 + text_size[0], y1), color, -1)
             cv2.putText(frame, label, (x1, y1), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1)
             
-            depth_map = self.depth_model.get_depth_image(frame)
+            #depth_map = self.depth_model.get_depth_image(frame)
             #depth_map = self.stereo_camera.get_depth(frame)
-            cv2.imshow('depth map - full', (depth_map * 255).astype(np.uint8))
+            #cv2.imshow('depth map - full', (depth_map * 255).astype(np.uint8))
             
             # Estimate plate proportions and print below bounding box
-            plate_proportion = 1
-            self.estimate_proportions(frame[y1:y2, x1:x2])
+            plate_proportion = self.estimate_proportions(frame[y1:y2, x1:x2])
             text = f'Est.: {plate_proportion:.2f}%'
             text_size, _ = cv2.getTextSize(text, cv2.FONT_HERSHEY_SIMPLEX, 0.5, 1)
             cv2.rectangle(frame, (x1, y2 - text_size[1]), (x1 + text_size[0], y2), color, -1)
